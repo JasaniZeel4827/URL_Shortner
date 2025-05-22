84388a03d263bbbbd7bf18ef51208da52d83c4bd
Zeel <deltapixel8a@gmail.com>
2025-05-22 19:33:02 +0530

    URL_Shortner

--- backend/src/controller/short_url.controller.js
+++ backend/src/controller/short_url.controller.js
@@ -14,3 +14,13 @@ export const createShortUrl = wrapAsync(async (req,res)=>{
     }
     res.status(200).json({shortUrl : process.env.APP_URL + shortUrl})
 })
+
+
+
+
+export const redirectFromShortUrl = wrapAsync(async (req,res)=>{
+    const {id} = req.params
+    const url = await getShortUrl(id)
+    if(!url) throw new Error("Short URL not found")
+    res.redirect(url.full_url)
+})
