5509a4a0c253e625d3074ecdb5015770c4666936
Zeel <deltapixel8a@gmail.com>
2025-05-22 19:31:10 +0530

    URL_Shortner

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -2,3 +2,10 @@ import { cookieOptions } from "../config/config.js"
 import { loginUser, registerUser } from "../services/auth.service.js"
 import wrapAsync from "../utils/tryCatchWrapper.js"
 
+export const register_user = wrapAsync( async (req, res) => {
+    const {name, email, password} = req.body
+    const {token,user} = await registerUser(name, email, password)
+    req.user = user
+    res.cookie("accessToken", token, cookieOptions)
+    res.status(200).json({message:"register success"})
+})
