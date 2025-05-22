3ebbbcefcc23d455b1b29f3e38f136bebf4d2cb8
Zeel <deltapixel8a@gmail.com>
2025-05-22 19:32:44 +0530

    URL_Shortner

--- backend/src/controller/short_url.controller.js
+++ backend/src/controller/short_url.controller.js
@@ -1,3 +1,16 @@
 import { getShortUrl } from "../dao/short_url.js"
 import { createShortUrlWithoutUser, createShortUrlWithUser } from "../services/short_url.service.js"
 import wrapAsync from "../utils/tryCatchWrapper.js"
+
+
+
+export const createShortUrl = wrapAsync(async (req,res)=>{
+    const data = req.body
+    let shortUrl
+    if(req.user){
+        shortUrl = await createShortUrlWithUser(data.url,req.user._id,data.slug)
+    }else{  
+        shortUrl = await createShortUrlWithoutUser(data.url)
+    }
+    res.status(200).json({shortUrl : process.env.APP_URL + shortUrl})
+})
