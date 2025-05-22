a89e5d9af6df1d5d5b4731c6dcd638e50c0e42ae
Zeel <deltapixel8a@gmail.com>
2025-05-22 19:38:10 +0530

    URL_Shortner

--- backend/src/dao/use.dao.js
+++ backend/src/dao/use.dao.js
@@ -10,3 +10,9 @@ export const findUserByEmail = async (email) => {
 export const findUserByEmailByPassword = async (email) => {
     return await User.findOne({email}).select('+password')
 }
+
+
+
+export const findUserById = async (id) => {
+    return await User.findById(id)
+}
