ab91748396bd645bf96aabe8d770ba04e7ae0160
Zeel <deltapixel8a@gmail.com>
2025-05-22 19:31:37 +0530

    URL_Shortner

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -9,3 +9,12 @@ export const register_user = wrapAsync( async (req, res) => {
     res.cookie("accessToken", token, cookieOptions)
     res.status(200).json({message:"register success"})
 })
+
+
+export const login_user = wrapAsync( async (req, res) => {
+    const {email, password} = req.body
+    const {token,user} = await loginUser(email, password)
+    req.user = user
+    res.cookie("accessToken", token, cookieOptions)
+    res.status(200).json({user:user,message:"login success"})
+})
